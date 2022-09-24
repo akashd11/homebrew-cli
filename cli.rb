90fa5642d47556640e1450a6b79a9337c786111a
Akash Deep <akashdeep@D11-Akash.Deep>
2022-09-25 01:12:54 +0530

    ased

--- cli.rb
+++ cli.rb
@@ -2,11 +2,10 @@
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Cli < Formula
-  include Language::Python::Virtualenv
   desc "testing brew cli"
   homepage "https://github.com/akashd11/cli"
-  url "https://github.com/akashd11/cli/archive/refs/tags/v1.3.tar.gz"
-  sha256 "5767aef5201794e39e55022aa53b16c5d9ea7c6f97f33a526fca2564cc8d874a"
+  url "https://github.com/akashd11/cli/archive/refs/tags/v1.4.tar.gz"
+  sha256 "69b87d497ac9283e80059290320f42fa50b13034110481b2b06865f23e4a8d61"
 
   depends_on "python@3.10"
 
