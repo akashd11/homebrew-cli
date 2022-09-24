9d973d9c347c55a31f403435a88616f3e4ea063a
Akash Deep <akashdeep@D11-Akash.Deep>
2022-09-25 00:38:45 +0530

    asdfff

--- cli.rb
+++ cli.rb
@@ -2,18 +2,17 @@
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Cli < Formula
-  include Language::Python::Virtualenv
-  desc "brew cli"
+  desc "testing brew cli"
   homepage "https://github.com/akashd11/cli"
-  url "https://github.com/akashd11/cli/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "30869eb3e08618104ceeeeb21001c69c70aeab2a510a02b31b444b527a54fef5"
+  url "https://github.com/akashd11/cli/archive/refs/tags/v1.3.tar.gz"
+  sha256 "5767aef5201794e39e55022aa53b16c5d9ea7c6f97f33a526fca2564cc8d874a"
 
-#    depends_on "python@3.10"
+  depends_on "python@3.10"
 
-  resource "cli" do
-    url "https://github.com/akashd11/cli/archive/refs/tags/v1.0.0.tar.gz"
-    sha256 "30869eb3e08618104ceeeeb21001c69c70aeab2a510a02b31b444b527a54fef5"
-  end
+   resource "typer" do
+       url "https://github.com/tiangolo/typer/archive/refs/tags/0.6.1.tar.gz"
+       sha256 "b082b64dd343836de6ad28e740a2335b83e3433c6877fabdf859bca63206b369"
+   end
 
   def install
     puts "before................"
@@ -21,5 +20,3 @@ class Cli < Formula
     puts "getting........................."
   end
 end
-
-
